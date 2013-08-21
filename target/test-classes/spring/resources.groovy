65c92e47e214b9c8527f471390cbcc22bf85a26a
morrisseyd <morrisseyd@gmail.com>
2013-08-21 17:44:10 +0100

    cleaned

--- target/test-classes/spring/resources.groovy
+++
@@ -1,8 +0,0 @@
-// Place your Spring DSL code here
-beans = {
-	customPropertyEditorRegistrar(CustomDateEditorRegistrar)
-	
-	//Custom bean for task status
-	taskStatusValues(java.util.HashMap, [Started: 'Started',
-		  NotStarted: 'Not Started', OnHold: 'On Hold'])
-	}
